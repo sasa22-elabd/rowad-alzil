6a77791538cb397cb39261112d77fddab708fa65
sasa22-elabd <livebein2@gmail.com>
2026-09-10 02:41:03 +0300

    تحويل قاعدة البيانات لـ MySQL

---
+++ gen-vapid.js
@@ -0,0 +1,3 @@
+const webpush = require("web-push");
+const keys = webpush.generateVAPIDKeys();
+console.log(keys);
--- models/index.js
+++ models/index.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "rowad.sqlite",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 // =========================
 // Admin
